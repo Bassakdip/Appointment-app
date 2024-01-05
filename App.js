a01542e9b89196c18f056a2e1d4a4dcd7c7746b6
Dipankar Basak <143477069+Bassakdip@users.noreply.github.com>
2024-01-05 20:37:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ async function saveData(event) {
     };
 
     try {
-        const res = await axios.post("https://crudcrud.com/api/109a064e403445edbbc1a3eec25932b0/appointmentData", obj);
+        const res = await axios.post("https://crudcrud.com/api/24a5b2be862a4525af736dfd573e3798/appointmentData", obj);
         console.log(res.data);
         showUserOnScreen(res.data);
     } catch (err) {
@@ -20,36 +20,51 @@ async function saveData(event) {
     }
 }
 
-window .addEventListener("DOMContentLoaded",async ()=>{
-    try{
-        const res = await axios.get("https://crudcrud.com/api/109a064e403445edbbc1a3eec25932b0/appointmentData")
-        for(var i=0;i<res.data.length;i++)
-        {
+window.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const res = await axios.get("https://crudcrud.com/api/24a5b2be862a4525af736dfd573e3798/appointmentData")
+        for (var i = 0; i < res.data.length; i++) {
             showUserOnScreen(res.data[i]);
         }
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
 })
 
 
 function showUserOnScreen(obj) {
-    
+
     const parentNode = document.getElementById('ul1');
     const childHTML = `<li id=${obj._id} > ${obj.name} - ${obj.email} - ${obj.phn}
-                        <button onclick= deleteDataFromCrudCrud('${obj._id}')>Delete</button>
-                        <button onclick= editUser('${obj.name}','${obj.email}','${obj.phn}')>Edit</button> 
+                        <button onclick=deleteDataFromCrudCrud('${obj._id}')>Delete</button>
+                        <button onclick="editUser('${obj.name}','${obj.email}','${obj.phn}','${obj._id}')">Edit</button> 
                         </li>`
-    parentNode.innerHTML = parentNode.innerHTML+childHTML;
+    parentNode.innerHTML = parentNode.innerHTML + childHTML;
+}
+
+async function editUser(name, email, phn, id) {
+
+    document.getElementById('nameInputTag').value = name;
+    document.getElementById('emailInputTag').value = email;
+    document.getElementById('phoneInputTag').value = phn;
+
+    deleteDataFromCrudCrud(id);
 }
 
 async function deleteDataFromCrudCrud(id) {
-    try{
-        const res = await axios.delete(`https://crudcrud.com/api/109a064e403445edbbc1a3eec25932b0/appointmentData/${id}`)
-        removeUserOnScreen(res.data);
+    try {
+        await axios.delete(`https://crudcrud.com/api/24a5b2be862a4525af736dfd573e3798/appointmentData/${id}`)
+        removeUserOnScreen(id);
     }
-    catch(err){
+    catch (err) {
         console.log(err)
+    }
+}
+function removeUserOnScreen(id) {
+    const parentaNode = document.getElementById('ul1');
+    const deletedNode = document.getElementById(id);
+    if (deletedNode) {
+        parentaNode.removeChild(deletedNode)
     }
 }
